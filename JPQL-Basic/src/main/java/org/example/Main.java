517a08ac89b87f43029941a326070bd096fbf109
조영래 <ip323@naver.com>
2023-12-08 13:48:21 +0900

    기본 문법과 쿼리 API

--- JPQL-Basic/src/main/java/org/example/Main.java
+++ JPQL-Basic/src/main/java/org/example/Main.java
@@ -1,9 +1,9 @@
 package org.example;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import org.example.jpql.Member;
+
+import javax.persistence.*;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,6 +14,28 @@ public class Main {
         tx.begin();
 
         try {
+            Member member = new Member();
+            member.setUsername("memberA");
+            member.setAge(10);
+            em.persist(member);
+
+            /*
+             * JPQL start
+             */
+            TypedQuery<Member> typedQuery = em.createQuery("select m from Member m", Member.class); //타입을 특정할 수 있을 때
+            Query query = em.createQuery("select m.username, m.age from Member m"); //타입을 특정할 수 없을 때
+
+            //get result
+            List<Member> typedQueryResultList = typedQuery.getResultList();
+            Member typedQuerySingleResult = typedQuery.getSingleResult(); //데이터가 하나만 나올 것이 확실한 경우, 없거나 둘 이상이면 예외 발생
+            List queryResultList = query.getResultList();
+
+            //parameter binding
+            TypedQuery<Member> typedQuery2 = em.createQuery("select m from Member m where m.username = :username", Member.class);
+            typedQuery2.setParameter("username", "memberA");
+            Member singleResult = typedQuery2.getSingleResult();
+            System.out.println("singleResult = " + singleResult.getClass());
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
