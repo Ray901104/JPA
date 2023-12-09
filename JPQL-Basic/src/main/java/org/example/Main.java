fa68e074f0886a10489619d47bf7416c065298a4
yrcho <ip323@naver.com>
2023-12-09 22:20:13 +0900

    조인

--- JPQL-Basic/src/main/java/org/example/Main.java
+++ JPQL-Basic/src/main/java/org/example/Main.java
@@ -1,11 +1,12 @@
 package org.example;
 
-import org.example.jpql.Address;
 import org.example.jpql.Member;
-import org.example.jpql.MemberDTO;
 import org.example.jpql.Team;
 
-import javax.persistence.*;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import java.util.List;
 
 public class Main {
@@ -17,72 +18,46 @@ public class Main {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("memberA");
-            member.setAge(10);
-            em.persist(member);
+            /**
+             * 조인
+             */
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            member1.setAge(10);
+            member1.setTeam(team);
+            em.persist(member1);
 
             em.flush();
             em.clear();
 
-            List<Member> result = em.createQuery("select m from Member m", Member.class)
-                    .getResultList(); //엔티티 조회
-
-            List<Team> resultTeam = em.createQuery("select t from Member m join m.team t", Team.class)
-                    .getResultList(); //join을 명시해주자(명시안해도 join이 나감)
-
-            List<Address> resultAddress = em.createQuery("select o.address from Order o", Address.class)
-                    .getResultList(); //embedded
-
-            /**
-             * 반환타입이 명시적이지 않을 때 사용
-             */
-            //첫번째 방법
-            List resultScala = em.createQuery("select distinct m.username, m.age from Member m")
-                    .getResultList(); //distinct + scala
-
-            Object o = resultScala.get(0);
-            Object[] objects = (Object[]) o;
-            System.out.println("objects[0] = " + objects[0]); //username
-            System.out.println("objects[1] = " + objects[1]); //age
-
-            //두번째 방법
-            List<Object[]> resultScala2 = em.createQuery("select distinct m.username, m.age from Member m")
-                    .getResultList();//distinct + scala
-
-            Object[] objects1 = resultScala2.get(0);
-            System.out.println("objects1[0] = " + objects1[0]);
-            System.out.println("objects1[1] = " + objects1[1]);
-
-            //세번째 방법 -> 가장 권장하는 방법!
-            List<MemberDTO> memberDTOS = em.createQuery("select new org.example.jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+            //내부조인
+            String qlString = "select m from Member m inner join m.team t"; //inner 생략가능
+            List<Member> resultInnerJoin = em.createQuery(qlString, Member.class)
                     .getResultList();
 
-            MemberDTO memberDTO = memberDTOS.get(0);
-            System.out.println("memberDTO.username = " + memberDTO.getUsername());
-            System.out.println("memberDTO.age = " + memberDTO.getAge());
-
-            /**
-             * 페이징 API
-             */
-            for (int i = 0; i < 100; i++) {
-                Member member1 = new Member();
-                member1.setUsername("member" + i);
-                member1.setAge(i);
-                em.persist(member1);
-            }
-
-            em.flush();
-            em.clear();
-
-            List<Member> resultPaging = em.createQuery("select m from Member m order by m.age desc", Member.class)
-                    .setFirstResult(0)
-                    .setMaxResults(10)
+            //외부조인
+            String qlString2 = "select m from Member m left outer join m.team t"; //outer 생략가능
+            List<Member> resultOuterJoin = em.createQuery(qlString2, Member.class)
                     .getResultList();
 
-            for (Member member1 : resultPaging) {
-                System.out.println("member1 = " + member1);
-            }
+            //세타조인
+            String qlString3 = "select m from Member m, Team t where m.username = t.name";
+            List<Member> resultThetaJoin = em.createQuery(qlString3, Member.class)
+                    .getResultList();
+
+            //조인 대상 필터링
+            String qlString4 = "select m from Member m left join m.team t on t.name = 'teamA'";
+            List<Member> resultFilterJoin = em.createQuery(qlString4, Member.class)
+                    .getResultList();
+
+            //연관관계 없는 엔티티 외부 조인
+            String qlString5 = "select m from Member m left join Team t on m.username = t.name";
+            List<Member> resultFilterOuterJoin = em.createQuery(qlString5, Member.class)
+                    .getResultList();
 
             tx.commit();
         } catch (Exception e) {
