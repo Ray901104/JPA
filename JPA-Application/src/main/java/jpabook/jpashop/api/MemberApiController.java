f498868e2fd985e5c89f9c86161fa74bf645de19
yrcho <ip323@naver.com>
2023-12-11 18:17:03 +0900

    회원 수정 API

--- JPA-Application/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ JPA-Application/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -4,11 +4,10 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,6 +29,15 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid UpdateMemberRequest request) {
+        memberService.update(id, request.getName());
+        Member foundMember = memberService.findOne(id);
+        return new UpdateMemberResponse(foundMember.getId(), foundMember.getName());
+    }
+
     /**
      * 단순 예제이므로 static inner class 생성 -> 실무에서는 별도 DTO!
      */
@@ -46,5 +54,17 @@ public class MemberApiController {
         public CreateMemberResponse(Long id) {
             this.id = id;
         }
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
     }
 }
