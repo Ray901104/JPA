71cc64258fdf2791ce90465b09c8f43f05a7defb
조영래 <ip323@naver.com>
2023-12-12 13:46:58 +0900

    간단한 주문 조회 V1: 엔티티를 직접 노출

--- JPA-Application/src/main/java/jpabook/jpashop/domain/Member.java
+++ JPA-Application/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
@@ -22,7 +23,7 @@ public class Member {
     @Embedded
     private Address address;
 
-    //@JsonIgnore //문제!
+    @JsonIgnore //문제!
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 }
