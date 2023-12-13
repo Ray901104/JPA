f5e603e415c99c41e2c236fc484880f933c60952
yrcho <ip323@naver.com>
2023-12-13 13:51:37 +0900

    주문 조회 V6: JPA에서 DTO로 직접 조회, 플랫 데이터 최적화

--- JPA-Application/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ JPA-Application/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -3,16 +3,18 @@ package jpabook.jpashop.repository.order.query;
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.OrderStatus;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@EqualsAndHashCode(of = "orderId")
 public class OrderQueryDto {
 
     private Long orderId;
     private String name;
-    private LocalDateTime orderData;
+    private LocalDateTime orderDate;
     private OrderStatus orderStatus;
     private Address address;
     private List<OrderItemQueryDto> orderitems;
@@ -20,8 +22,17 @@ public class OrderQueryDto {
     public OrderQueryDto(Long orderId, String name, LocalDateTime orderData, OrderStatus orderStatus, Address address) {
         this.orderId = orderId;
         this.name = name;
-        this.orderData = orderData;
+        this.orderDate = orderData;
         this.orderStatus = orderStatus;
         this.address = address;
+    }
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, List<OrderItemQueryDto> orderitems) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.orderitems = orderitems;
     }
 }
