66080face711e32f25dacf5ab5089d11ea66356d
조영래 <ip323@naver.com>
2023-12-14 10:53:35 +0900

    스프링 데이터 JPA와 DB 설정, 동작확인 p6spy 추가

---
+++ SpringData-JPA/src/test/java/spring/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,33 @@
+package spring.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import spring.datajpa.entity.Member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class MemberJpaRepositoryTest {
+
+    @Autowired MemberJpaRepository memberJpaRepository;
+
+    @Test
+    void testMember() {
+        //given
+        Member member = new Member("memberA");
+
+        //when
+        Member savedMember = memberJpaRepository.save(member);
+
+        //then
+        Member foundMember = memberJpaRepository.find(savedMember.getId());
+        assertThat(foundMember.getId()).isEqualTo(member.getId());
+        assertThat(foundMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(foundMember).isEqualTo(member);
+    }
+}
