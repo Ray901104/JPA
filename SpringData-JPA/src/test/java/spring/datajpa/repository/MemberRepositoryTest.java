312d521b9029aa1c1c5040de2b11ca2a9ffad129
조영래 <ip323@naver.com>
2023-12-14 13:25:18 +0900

    공통 인터페이스 적용

--- SpringData-JPA/src/test/java/spring/datajpa/repository/MemberRepositoryTest.java
+++ SpringData-JPA/src/test/java/spring/datajpa/repository/MemberRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import spring.datajpa.entity.Member;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -18,7 +20,7 @@ class MemberRepositoryTest {
 
     @Test
     void testMember() {
-        System.out.println("memberRepository.getClass() = " + memberRepository.getClass());
+        System.out.println("memberRepository.getClass() = " + memberRepository.getClass()); //프록시 객체
         //given
         Member member = new Member("memberA");
 
@@ -30,5 +32,37 @@ class MemberRepositoryTest {
         assertThat(foundMember.getId()).isEqualTo(member.getId());
         assertThat(foundMember.getUsername()).isEqualTo(member.getUsername());
         assertThat(foundMember).isEqualTo(member);
+    }
+
+    @Test
+    void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //단건 조회 검증
+        Member foundMember1 = memberRepository.findById(member1.getId()).orElse(new Member("member1"));
+        Member foundMember2 = memberRepository.findById(member2.getId()).orElse(new Member("member2"));
+        assertThat(foundMember1).isEqualTo(member1);
+        assertThat(foundMember2).isEqualTo(member2);
+
+        //update 메소드가 필요없는 이유 -> 변경감지
+        foundMember1.setUsername("member!!!!!!");
+
+        //리스트 조회 검증
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        //카운트 검증
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        //삭제 검증
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        long deletedCount = memberRepository.count();
+        assertThat(deletedCount).isEqualTo(0);
     }
 }
