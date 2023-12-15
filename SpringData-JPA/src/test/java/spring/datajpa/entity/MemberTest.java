2d623c2b4bca9fc34d08847bbb42acf2bcba6818
조영래 <ip323@naver.com>
2023-12-15 14:34:34 +0900

    순수 JPA를 사용한 Auditing

--- SpringData-JPA/src/test/java/spring/datajpa/entity/MemberTest.java
+++ SpringData-JPA/src/test/java/spring/datajpa/entity/MemberTest.java
@@ -3,9 +3,11 @@ package spring.datajpa.entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import spring.datajpa.repository.MemberRepository;
 
 import java.util.List;
 
@@ -16,6 +18,9 @@ class MemberTest {
 
     @PersistenceContext
     EntityManager em;
+
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     void testEntity() {
@@ -46,5 +51,25 @@ class MemberTest {
             System.out.println("member = " + member);
             System.out.println("-> member.team = " + member.getTeam());
         }
+    }
+
+    @Test
+    public void jpaEventBaseEntityTest() throws Exception {
+        //given
+        Member member = new Member("member1");
+        memberRepository.save(member); //@PrePersist
+
+        Thread.sleep(1000);
+        member.setUsername("member2");
+
+        em.flush(); //@PreUpdate
+        em.clear();
+
+        //when
+        Member foundMember = memberRepository.findById(member.getId()).get();
+
+        //then
+        System.out.println("foundMember.createdDate = " + foundMember.getCreatedDate());
+        System.out.println("foundMember.updatedDate = " + foundMember.getUpdatedDate());
     }
 }
