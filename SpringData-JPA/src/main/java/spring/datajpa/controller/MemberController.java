6ea269630c9ba2432db43c7172d26994804dca6d
조영래 <ip323@naver.com>
2023-12-15 15:15:13 +0900

    Web 확장 - 도메인 클래스 컨버터

---
+++ SpringData-JPA/src/main/java/spring/datajpa/controller/MemberController.java
@@ -0,0 +1,33 @@
+package spring.datajpa.controller;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import spring.datajpa.entity.Member;
+import spring.datajpa.repository.MemberRepository;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).orElse(new Member("member1"));
+        return member.getUsername();
+    }
+
+    //단순 조회용으로 사용 -> 트랜잭션이 없는 범위에서 엔티티를 조회한 것이기 때문에
+    @GetMapping("/members2/{id}")
+    public String findMember2(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("userA"));
+    }
+}
